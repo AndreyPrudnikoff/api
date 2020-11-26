b7ab65eca411a159c89600ae2cabd139d53a2e72
AndreyPrudnikoff <a.prudniks@gmail.com>
2020-11-26 12:34:29 +0200

    file

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require('cors')
-
+const multer = require('multer')
 const corsOptions = {
-    origin: 'http://localhost:63342',
+    origin: 'http://localhost:8080',
     optionsSuccessStatus: 200
 }
-
+app.use(multer({dest:"uploads"}).single("file"));
 app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.post('/register',(req, res) => {
-    let answer = req.body.name
+app.post('/api/excel',(req, res) => {
+    let answer = req.file
     console.log(answer)
-    res.send(JSON.stringify({"name": "MyName"}))
+    answer ? res.send(answer.originalname) : res.send(404)
 });
 
-app.listen(3000)
+app.listen(8000)
 console.log('Server starting...')
+
